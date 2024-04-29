4261fc0222180e9f36c0c2c4e159899e3969e951
sunwoong <sunwoong1122@gmail.com>
2024-04-29 23:24:56 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/practice/cafekiosk/spring/CafekioskApplication.java
@@ -0,0 +1,11 @@
+package practice.cafekiosk.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafekioskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CafekioskApplication.class, args);
+    }
+}
---
+++ src/test/java/practice/cafekiosk/CafekioskApplicationTests.java
@@ -0,0 +1,11 @@
+package practice.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafekioskApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
