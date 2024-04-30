17b54274d6f48b921ed4e45c24bc396e776744ac
sunwoong <sunwoong1122@gmail.com>
2024-04-30 19:23:51 +0900

    test: 주문 기능 테스트 어려운 영역 통합 & 분리 코드 작성

--- src/test/java/practice/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/practice/cafekiosk/unit/CafeKioskTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import practice.cafekiosk.unit.beverage.Americano;
 import practice.cafekiosk.unit.beverage.Beverage;
 import practice.cafekiosk.unit.beverage.Latte;
+import practice.cafekiosk.unit.order.Order;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -81,5 +83,52 @@ class CafeKioskTest {
 
         // then
         assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @Test
+    void createOrderWithBasic() {
+        // given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano, 1);
+
+        // when
+        Order order = cafeKiosk.createOrder();
+
+        // then
+        List<Beverage> beverages = order.getBeverages();
+        assertThat(beverages).hasSize(1);
+        Beverage findAmericano = beverages.get(0);
+        assertThat(findAmericano.getName()).isEqualTo("americano");
+    }
+
+    @Test
+    void createOrderWithRefactoring() {
+        // given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano, 1);
+
+        // when
+        Order order = cafeKiosk.createOrder(LocalDateTime.of(2024, 4, 30, 10, 0));
+
+        // then
+        List<Beverage> beverages = order.getBeverages();
+        assertThat(beverages).hasSize(1);
+        Beverage findAmericano = beverages.get(0);
+        assertThat(findAmericano.getName()).isEqualTo("americano");
+    }
+
+    @Test
+    void createOrderOutsideOpenTime() {
+        // given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano, 1);
+
+        // when & then
+        assertThatThrownBy(() -> cafeKiosk.createOrder(LocalDateTime.of(2024, 4, 30, 9, 59)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문 시간이 아닙니다. 관리자에게 문의하세요.");
     }
 }
