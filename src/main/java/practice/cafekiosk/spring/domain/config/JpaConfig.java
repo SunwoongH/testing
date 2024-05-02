821ca9965f30cc0105e1efe704b746a29b54adc3
sunwoong <sunwoong1122@gmail.com>
2024-05-02 15:18:23 +0900

    chore: jpa auditing 설정

---
+++ src/main/java/practice/cafekiosk/spring/domain/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package practice.cafekiosk.spring.domain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
