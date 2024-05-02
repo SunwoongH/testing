845aa8c5b51a75f39601ff70857e6c127fbcf06d
sunwoong <sunwoong1122@gmail.com>
2024-05-02 15:19:06 +0900

    feat: product response 구현

---
+++ src/main/java/practice/cafekiosk/spring/api/dto/response/ProductResponse.java
@@ -0,0 +1,29 @@
+package practice.cafekiosk.spring.api.dto.response;
+
+import com.zaxxer.hikari.pool.ProxyDatabaseMetaData;
+import lombok.AccessLevel;
+import lombok.Builder;
+import practice.cafekiosk.spring.domain.product.Product;
+import practice.cafekiosk.spring.domain.product.ProductSellingStatus;
+import practice.cafekiosk.spring.domain.product.ProductType;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record ProductResponse(
+        Long id,
+        String productNumber,
+        ProductType type,
+        ProductSellingStatus sellingStatus,
+        String name,
+        int price
+) {
+    public static ProductResponse of(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .productNumber(product.getProductNumber())
+                .type(product.getType())
+                .sellingStatus(product.getSellingStatus())
+                .name(product.getName())
+                .price(product.getPrice())
+                .build();
+    }
+}
