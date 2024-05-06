b802d77cd2d43a6317561dbaa0cc25b3426a70da
sunwoong <sunwoong1122@gmail.com>
2024-05-06 19:15:36 +0900

    feat: 주문 api 응답 dto 구현

---
+++ src/main/java/practice/cafekiosk/spring/api/dto/response/OrderResponse.java
@@ -0,0 +1,28 @@
+package practice.cafekiosk.spring.api.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import practice.cafekiosk.spring.domain.order.Order;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record OrderResponse(
+        Long id,
+        int totalPrice,
+        LocalDateTime registeredDateTime,
+        List<ProductResponse> products
+) {
+    public static OrderResponse of(Order order) {
+        return OrderResponse.builder()
+                .id(order.getId())
+                .totalPrice(order.getTotalPrice())
+                .registeredDateTime(order.getRegisteredDateTime())
+                .products(order.getOrderProducts()
+                        .stream()
+                        .map(orderProduct -> ProductResponse.of(orderProduct.getProduct()))
+                        .toList())
+                .build();
+    }
+}
