f3cefcd7e0d16d7807aa35a21dead03c9361ec4c
sunwoong <sunwoong1122@gmail.com>
2024-05-06 19:16:22 +0900

    feat: 상품 & 주문 엔티티 구현

---
+++ src/main/java/practice/cafekiosk/spring/domain/orderproduct/OrderProduct.java
@@ -0,0 +1,48 @@
+package practice.cafekiosk.spring.domain.orderproduct;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import practice.cafekiosk.spring.domain.BaseEntity;
+import practice.cafekiosk.spring.domain.order.Order;
+import practice.cafekiosk.spring.domain.product.Product;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class OrderProduct extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "orderproduct_id")
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Builder
+    private OrderProduct(Order order, Product product) {
+        this.order = order;
+        this.product = product;
+    }
+
+    public static OrderProduct create(Order order, Product product) {
+        OrderProduct orderProduct = OrderProduct.builder()
+                .product(product)
+                .build();
+        orderProduct.changeOrderProduct(order);
+        return orderProduct;
+    }
+
+    private void changeOrderProduct(Order order) {
+        if (this.order != null) {
+            order.removeOrderProduct(this);
+        }
+        this.order = order;
+        order.addOrderProduct(this);
+    }
+}
