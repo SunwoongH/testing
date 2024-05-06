40554bddeb333a2f159c78456c10acbe1d501337
sunwoong <sunwoong1122@gmail.com>
2024-05-06 19:15:12 +0900

    test: 상품 조회 쿼리 테스트 코드 작성

--- src/test/java/practice/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/practice/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -1,7 +1,5 @@
 package practice.cafekiosk.spring.domain.product;
 
-import org.assertj.core.api.Assertions;
-import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +7,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 
 @ActiveProfiles("test")
 @DataJpaTest
@@ -21,38 +22,49 @@ class ProductRepositoryTest {
     @Test
     void findAllBySellingStatusIn() {
         // given
-        Product product1 = Product.builder()
-                .productNumber("001")
-                .type(ProductType.HANDMADE)
-                .sellingStatus(ProductSellingStatus.SELLING)
-                .name("아메리카노")
-                .price(4000)
-                .build();
-        Product product2 = Product.builder()
-                .productNumber("002")
-                .type(ProductType.HANDMADE)
-                .sellingStatus(ProductSellingStatus.HOLD)
-                .name("카페라떼")
-                .price(4500)
-                .build();
-        Product product3 = Product.builder()
-                .productNumber("003")
-                .type(ProductType.HANDMADE)
-                .sellingStatus(ProductSellingStatus.STOP_SELLING)
-                .name("팥빙수")
-                .price(7000)
-                .build();
+        Product product1 = Product.createProduct("001",
+                ProductType.HANDMADE, ProductSellingStatus.SELLING, "아메리카노", 4000);
+        Product product2 = Product.createProduct("002",
+                ProductType.HANDMADE, ProductSellingStatus.HOLD, "카페라떼", 4500);
+        Product product3 = Product.createProduct("003",
+                ProductType.HANDMADE, ProductSellingStatus.STOP_SELLING, "팥빙수", 7000);
         productRepository.saveAll(List.of(product1, product2, product3));
 
         // when
         List<Product> products = productRepository.findAllBySellingStatusIn(List.of(ProductSellingStatus.SELLING, ProductSellingStatus.HOLD));
 
         // then
-        Assertions.assertThat(products).hasSize(2)
+        assertThat(products)
+                .hasSize(2)
                 .extracting("productNumber", "name", "sellingStatus")
                 .containsExactlyInAnyOrder(
-                        Tuple.tuple("001", "아메리카노", ProductSellingStatus.SELLING),
-                        Tuple.tuple("002", "카페라떼", ProductSellingStatus.HOLD)
+                        tuple("001", "아메리카노", ProductSellingStatus.SELLING),
+                        tuple("002", "카페라떼", ProductSellingStatus.HOLD)
+                );
+    }
+
+    @DisplayName("상품번호 리스트로 상품들을 조회한다.")
+    @Test
+    void findAllByProductNumberIn() {
+        // given
+        Product product1 = Product.createProduct("001",
+                ProductType.HANDMADE, ProductSellingStatus.SELLING, "아메리카노", 4000);
+        Product product2 = Product.createProduct("002",
+                ProductType.HANDMADE, ProductSellingStatus.HOLD, "카페라떼", 4500);
+        Product product3 = Product.createProduct("003",
+                ProductType.HANDMADE, ProductSellingStatus.STOP_SELLING, "팥빙수", 7000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        List<Product> products = productRepository.findAllByProductNumberIn(List.of("001", "002"));
+
+        // then
+        assertThat(products)
+                .hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("001", "아메리카노", ProductSellingStatus.SELLING),
+                        tuple("002", "카페라떼", ProductSellingStatus.HOLD)
                 );
     }
 }
