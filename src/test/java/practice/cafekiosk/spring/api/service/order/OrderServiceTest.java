d6213c860e110ad04c37d720ef6b5729c3efdc04
sunwoong <sunwoong1122@gmail.com>
2024-05-06 19:10:24 +0900

    test: service layer 주문 생성 테스트 코드 작성

---
+++ src/test/java/practice/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -0,0 +1,59 @@
+package practice.cafekiosk.spring.api.service.order;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import practice.cafekiosk.spring.api.dto.request.OrderCreateRequest;
+import practice.cafekiosk.spring.api.dto.response.OrderResponse;
+import practice.cafekiosk.spring.domain.product.Product;
+import practice.cafekiosk.spring.domain.product.ProductRepository;
+import practice.cafekiosk.spring.domain.product.ProductSellingStatus;
+import practice.cafekiosk.spring.domain.product.ProductType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class OrderServiceTest {
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    ProductRepository productRepository;
+
+    @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrder() {
+        // given
+        Product product1 = Product.createProduct("001",
+                ProductType.HANDMADE, ProductSellingStatus.SELLING, "아메리카노", 4000);
+        Product product2 = Product.createProduct("002",
+                ProductType.HANDMADE, ProductSellingStatus.HOLD, "카페라떼", 4500);
+        Product product3 = Product.createProduct("003",
+                ProductType.HANDMADE, ProductSellingStatus.STOP_SELLING, "팥빙수", 7000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+        OrderCreateRequest request = new OrderCreateRequest(List.of("001", "002"));
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        // when
+        OrderResponse response = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(response.id()).isNotNull();
+        assertThat(response)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 8500);
+        assertThat(response.products())
+                .hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 4000),
+                        tuple("002", 4500)
+                );
+    }
+}
