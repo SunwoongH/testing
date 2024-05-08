9c25825217d4a186eac827476b6de7295a9c8e00
sunwoong <sunwoong1122@gmail.com>
2024-05-08 22:53:10 +0900

    test: 상품번호 조회 쿼리 테스트 코드 작성

--- src/test/java/practice/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/practice/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -67,4 +67,34 @@ class ProductRepositoryTest {
                         tuple("002", "카페라떼", ProductSellingStatus.HOLD)
                 );
     }
+
+    @DisplayName("가장 마지막으로 저장한 상품의 상품번호를 읽어온다.")
+    @Test
+    void findLatestProductNumber() {
+        // given
+        String targetProductNumber = "003";
+        Product product1 = Product.create("001",
+                ProductType.HANDMADE, ProductSellingStatus.SELLING, "아메리카노", 4000);
+        Product product2 = Product.create("002",
+                ProductType.HANDMADE, ProductSellingStatus.HOLD, "카페라떼", 4500);
+        Product product3 = Product.create("003",
+                ProductType.HANDMADE, ProductSellingStatus.STOP_SELLING, "팥빙수", 7000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        // then
+        assertThat(latestProductNumber).isEqualTo(targetProductNumber);
+    }
+
+    @DisplayName("가장 마지막으로 저장한 상품의 상품번호를 읽어올 때, 상품이 하나도 없는 경우에는 null을 반환한다.")
+    @Test
+    void findLatestProductNumberWhenProductIsEmpty() {
+        // when
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        // then
+        assertThat(latestProductNumber).isNull();
+    }
 }
