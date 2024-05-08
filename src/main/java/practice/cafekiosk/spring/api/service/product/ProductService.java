ae5d8981aecfb9e0985869bfbf67e0381059c6e6
sunwoong <sunwoong1122@gmail.com>
2024-05-08 22:54:06 +0900

    feat: 상품 생성 비즈니스 로직 구현

--- src/main/java/practice/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/practice/cafekiosk/spring/api/service/product/ProductService.java
@@ -3,7 +3,9 @@ package practice.cafekiosk.spring.api.service.product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import practice.cafekiosk.spring.api.dto.request.ProductCreateRequest;
 import practice.cafekiosk.spring.api.dto.response.ProductResponse;
+import practice.cafekiosk.spring.domain.product.Product;
 import practice.cafekiosk.spring.domain.product.ProductRepository;
 import practice.cafekiosk.spring.domain.product.ProductSellingStatus;
 
@@ -15,10 +17,29 @@ import java.util.List;
 public class ProductService {
     private final ProductRepository productRepository;
 
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+        Product product = Product.create(nextProductNumber, request.type(),
+                request.sellingStatus(), request.name(), request.price());
+        Product savedProduct = productRepository.save(product);
+        return ProductResponse.of(savedProduct);
+    }
+
     public List<ProductResponse> getSellingProducts() {
         return productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay())
                 .stream()
                 .map(ProductResponse::of)
                 .toList();
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+        return String.format("%03d", nextProductNumberInt);
     }
 }
