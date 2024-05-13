5b121c678d51371e81d07c59373f8bc52e6c4897
sunwoong <sunwoong1122@gmail.com>
2024-05-13 22:17:54 +0900

    feat: 주문 통계 비즈니스 로직 구현

---
+++ src/main/java/practice/cafekiosk/spring/api/service/order/OrderStatisticsService.java
@@ -0,0 +1,39 @@
+package practice.cafekiosk.spring.api.service.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import practice.cafekiosk.spring.api.service.mail.MailService;
+import practice.cafekiosk.spring.domain.order.Order;
+import practice.cafekiosk.spring.domain.order.OrderRepository;
+import practice.cafekiosk.spring.domain.order.OrderStatus;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class OrderStatisticsService {
+    private final OrderRepository orderRepository;
+    private final MailService mailService;
+
+    public boolean sendOrderStatisticsMail(LocalDate orderDate, String email) {
+        List<Order> orders = orderRepository.findOrdersBy(
+                orderDate.atStartOfDay(),
+                orderDate.plusDays(1).atStartOfDay(),
+                OrderStatus.PAYMENT_COMPLETED
+        );
+        int totalAmount = orders.stream()
+                .mapToInt(Order::getTotalPrice)
+                .sum();
+        boolean result = mailService.sendMail(
+                "test@test.com"
+                , email,
+                String.format("[매출통계] %s", orderDate),
+                String.format("총 매출 합계는 %s원 입니다.", totalAmount)
+        );
+        if (!result) {
+            throw new IllegalArgumentException("매출 통계 메일 전송에 실패했습니다.");
+        }
+        return true;
+    }
+}
